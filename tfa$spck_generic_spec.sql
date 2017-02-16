b8814571f7e5470899b1ae3494e7c7a0662f6332
OracleAtHMC <jwitteman@hmc-heerema.com>
2017-02-16 09:19:09 +0100

    This could work..

---
+++ tfa$spck_generic_spec.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE PACKAGE APEX_TFA."TFA$SPCK_GENERIC" IS
+/*
+** Created    : 15-FEB-2017
+** Creator    : Job Witteman
+**
+** Usage:
+** ----------
+** This contains the Functions/Procedures to operate on DPR Projects
+**
+** 1.  GET_STUFF
+**     This function will retrieve stuff.
+*/
+
+FUNCTION get_stuff
+( pi_input IN VARCHAR2
+)
+RETURN VARCHAR2
+;
+
+END TFA$SPCK_GENERIC
+;
+/
